98cef6f82c393ff40ee094e90840f9083276154b
rubenrua <rubenrua@teltek.es>
2018-10-18 12:59:10 +0200

    Use apcu in index

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -21,4 +21,39 @@ class IndexController extends Base
         $this->get('pumukit_web_tv.breadcrumbs')->reset();
         return array('event' => $event);
     }
+
+
+    /**
+     * @Template("PumukitWebTVBundle:Index:mostviewed.html.twig")
+     */
+    public function mostviewedlastmonthAction()
+    {
+        $apcuKey = 'cmar-lastmonth';
+        $apcuTTL = 3 * 60 * 60;
+
+        if (extension_loaded('apcu')) {
+            $ids = apcu_fetch($apcuKey);
+            $dm = $this->get('doctrine_mongodb.odm.document_manager');
+            $repo = $dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+
+            $multimediaObjectsSortedByNumview = $repo->findBy(array('_id' => array('$in' => $ids)));
+
+            if ($multimediaObjectsSortedByNumview) {
+                return array('multimediaObjectsSortedByNumview' => $multimediaObjectsSortedByNumview);
+            }
+        }
+
+        $multimediaObjectsSortedByNumview = $this->get('pumukit_stats.stats')->getMostViewedUsingFilters(30, 3);
+
+        if (extension_loaded('apcu')) {
+            $ids = array();
+            foreach ($multimediaObjectsSortedByNumview as $mm) {
+                $ids[] = $mm->getId();
+            }
+
+            apcu_store($apcuKey, $ids, $apcuTTL);
+        }
+
+        return array('multimediaObjectsSortedByNumview' => $multimediaObjectsSortedByNumview);
+    }
 }
