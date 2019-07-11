d6636af11c908e138f37b82b582c6f524244aa9c
rubenrua <rubenrua@teltek.es>
2019-07-11 13:40:09 +0200

    TTK-18691 change default value of inbox_depth to false

--- DependencyInjection/PumukitCmarWebTVExtension.php
+++ DependencyInjection/PumukitCmarWebTVExtension.php
@@ -29,5 +29,8 @@ class PumukitCmarWebTVExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
     }
 }
